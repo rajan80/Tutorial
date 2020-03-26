98f9617c1cbac5998bf12b38afd0523d64cb3ade
rajan80 <rajanshrestha80@gmail.com>
2020-03-26 14:10:03 -0500

    My code ==== yay

---
+++ Tutorial1/src/tutorialTest/test.java
@@ -0,0 +1,12 @@
+package tutorialTest;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s= "Rajan";
+		System.out.println(s);
+
+	}
+
+}
